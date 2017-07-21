99cabb4ec8ca94fc604d6ba1fcd208dc50d1c17f
Tanu <tanubhadwal4@gmail.com>
2017-07-21 10:10:19 +0530

    signup and login file added

---
+++ instagram/demoapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from models import UserModel
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = UserModel
+        fields = ['email', 'username', 'name', 'password']
+
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = UserModel
+        fields = ['username', 'password']
